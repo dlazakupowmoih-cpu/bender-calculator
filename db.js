ce403ad16c7e8d7de78e8160d480e20ec158df65
dlazakupowmoih-cpu <dlazakupowmoih@gmail.com>
2026-09-13 12:01:53 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,26 +1,26 @@
 const conduitSpecs = {
     emt: {
-        "1/2":   { takeup: 5,     clr: 4.1875 },
-        "3/4":   { takeup: 6,     clr: 4.375 },
-        "1":     { takeup: 8,     clr: 5.75 },
-        "1-1/4": { takeup: 11,    clr: 7.25 },
-        "1-1/2": { takeup: 13,    clr: 8.25 },
-        "2":     { takeup: 15,    clr: 9.5 }
+        "1/2":   { takeup: 5,     clr: 4 },
+        "3/4":   { takeup: 6,     clr: 5 },
+        "1":     { takeup: 8,     clr: 8 },
+        "1-1/4": { takeup: 11,    clr: 11 },
+        "1-1/2": { takeup: 14,    clr: 14 },
+        "2":     { takeup: 18,    clr: 18 }
     },
     imc: {
         "1/2":   { takeup: 6,     clr: 4.5 },
-        "3/4":   { takeup: 6.5,   clr: 5.0 },
-        "1":     { takeup: 8.5,   clr: 6.25 },
-        "1-1/4": { takeup: 11.5,  clr: 7.75 },
-        "1-1/2": { takeup: 13.5,  clr: 8.75 },
-        "2":     { takeup: 16,    clr: 10.0 }
+        "3/4":   { takeup: 7,     clr: 5.5 },
+        "1":     { takeup: 9,     clr: 8.5 },
+        "1-1/4": { takeup: 12,    clr: 12 },
+        "1-1/2": { takeup: 15,    clr: 15 },
+        "2":     { takeup: 19,    clr: 19 }
     },
     rigid: {
         "1/2":   { takeup: 6,     clr: 4.5 },
-        "3/4":   { takeup: 6.5,   clr: 5.0 },
-        "1":     { takeup: 8.5,   clr: 6.25 },
-        "1-1/4": { takeup: 11.5,  clr: 7.75 },
-        "1-1/2": { takeup: 13.5,  clr: 8.75 },
-        "2":     { takeup: 16,    clr: 10.0 }
+        "3/4":   { takeup: 7,     clr: 5.5 },
+        "1":     { takeup: 9,     clr: 8.5 },
+        "1-1/4": { takeup: 12,    clr: 12 },
+        "1-1/2": { takeup: 15,    clr: 15 },
+        "2":     { takeup: 19,    clr: 19 }
     }
 };
